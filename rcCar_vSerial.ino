53e377bfacd8d1f22d251d3292f78fd2973c7751
Kim-DongGyu <kerbers_@naver.com>
2017-05-21 20:03:19 +0900

    Update rcCar_vSerial.ino
    전진,후진,왼쪽,오른쪽,정지 기능 추가

--- rcCar_vSerial.ino
+++ rcCar_vSerial.ino
@@ -1,88 +1,73 @@
 #include <SoftwarwSerial.h>
 
+//모터에서 아두이노에 연결한 번호
 int A_1A = 6;
 int A_1B = 11;
 int B_1A = 3;
 int B_1B = 5;
 
-int btRxPin = 12;
-int btTxPin = 13;
-
 void setup()
 {
   Serial.begin(9600);
-  pinMode(ENABLEPIN, OUTPUT);
-  pinMode(SERVOPIN, OUTPUT);
-  pinMode(DCPIN_P, OUTPUT);
-  pinMode(DCPIN_M, OUTPUT);
-  servo.attach(SERVOPIN);
-  reset();
-
-  Serial.print("W:forward S:backward A:left D:right SPACE:stop\nQ:speed+ E:speed-");
+  pinMode(A_1A, OUTPUT);
+  pinMode(A_1B, OUTPUT);
+  pinMode(B_1A, OUTPUT);
+  pinMode(B_1B, OUTPUT);
+  digitalWrite(A_1A, LOW);
+  digitalWrite(A_1B, LOW);
+  digitalWrite(B_1A, LOW);
+  digitalWrite(B_1B, LOW);  
 }
 
-void loop()
-{
-  while(Serial.available()){
-    input = Serial.read();
-    switch(input)
-    {
-      case 'w':
-      digitalWrite(DCPIN_P, HIGH);
-      digitalWrite(DCPIN_M, LOW);
+void loop() {
+  /*
+    'f' - 전진명령(forward)
+    'b' - 후진명령(back)
+    'l' - 왼쪽명령(left)
+    'r' - 오른쪽명령(right)
+    's' - 정지명령(stop)
+  */  
+  switch (cmd) {
+    case 'f':
+      //모터A 정회전
+      analogWrite(A_1A, speed);
+      analogWrite(A_1B, 0);
+      //모터B 정회전
+      analogWrite(B_1A, speed);
+      analogWrite(B_1B, 0);
       break;
-
-      case 32:
-      digitalWrite(DCPIN_P, LOW);
-      digitalWrite(DCPIN_M, LOW);
+    case 'b':
+      //모터A 역회전
+      analogWrite(A_1A, 0);
+      analogWrite(A_1B, speed);
+      //모터B 역회전
+      analogWrite(B_1A, 0);
+      analogWrite(B_1B, speed);
       break;
-      //space
-
-      case 's':
-      digitalWrite(DCPIN_P, LOW);
-      digitalWrite(DCPIN_M, HIGH);
+    case 'r':
+      //모터A 정회전
+      analogWrite(A_1A, 0);
+      analogWrite(A_1B, 0);
+      //모터B 역회전
+      analogWrite(B_1A, speed);
+      analogWrite(B_1B, 0);
       break;
-
-      case 'a':
-      angle -= 10;
+    case 'l':
+      //모터A 역회전
+      analogWrite(A_1A, speed);
+      analogWrite(A_1B, 0);
+      //모터B 정회전
+      analogWrite(B_1A, 0);
+      analogWrite(B_1B, 0);
       break;
-
-      case 'd':
-      angle += 10;
+    case 's':
+      //모터A 정지
+      analogWrite(A_1A, 0);
+      analogWrite(A_1B, 0);
+      //모터B 정지
+      analogWrite(B_1A, 0);
+      analogWrite(B_1B, 0);
       break;
-
-      case 'q':
-      if(speeds > 235)
-        speeds = 255;
-      else
-        speeds += 20;
-      break;
-
-      case 'e':
-      if(speeds < 100)
-        speeds = 80;
-      else
-        speeds -= 20;
-      break;
-
-      case 'f':
-      reset();
-      break;
-
-      default:
-      break;
-    }
-    servo.write(angle);
-    analogWrite(ENABLEPIN, speeds);
   }
 }
 
-void reset()
-{
-  angle = 115;
-  speeds = 130;
-  digitalWrite(DCPIN_P, LOW);
-  digitalWrite(DCPIN_M, LOW);
-  servo.write(angle);
-  analogWrite(ENABLEPIN, speeds);
-}
